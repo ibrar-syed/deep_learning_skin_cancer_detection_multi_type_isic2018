77dac2b51991148de67767f9d158d795ff561b63
Syed Ibrar Hussain <syedhussain.ibrar@gmail.com>
2025-08-05 14:33:30 +0200

    Update and rename inceptionnetv3.py to inceptionv3.py

--- models/inceptionv3.py
+++ models/inceptionv3.py
@@ -1,3 +1,4 @@
+#iv3
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, Dense, Dropout, GlobalAveragePooling2D
 from tensorflow.keras.applications import InceptionV3
