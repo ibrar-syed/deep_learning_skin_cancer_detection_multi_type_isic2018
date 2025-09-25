c4374e563749d08318b37b6df50c8235fd6516e5
Syed <syedhussain.ibrar@gmail.com>
2025-09-25 15:57:22 +0200

    Update nasnetmobile.py

--- models/nasnetmobile.py
+++ models/nasnetmobile.py
@@ -11,12 +11,13 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-#
+##
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ##for the skin cancer detection with multi-layer structure##for the skin cancer detection with multi-layer structure
 from tensorflow.keras.models import Model
+from tensorflow.keras.models import Models
 from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D, Input
 from tensorflow.keras.applications import NASNetMobile
 from tensorflow.keras.optimizers import Adam
