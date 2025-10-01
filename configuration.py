f6ed6644e05fc299192ce4fbeb3a109f5e48c943
Syed <syedhussain.ibrar@gmail.com>
2025-10-01 14:32:28 +0200

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -1,6 +1,6 @@
 ## Copyright (C) 2025 ibrar-syed <syed.ibraras@gmail.com>
 # This file is part of the Skin_Cancer Detection Project
-#####
+##
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
