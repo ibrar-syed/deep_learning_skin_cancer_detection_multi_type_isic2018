62bdfe77706038c8b7cc33387305eb40edefa8ff
Syed <syedhussain.ibrar@gmail.com>
2025-10-01 14:34:33 +0200

    Update coatnet.py

--- models/coatnet.py
+++ models/coatnet.py
@@ -1,6 +1,5 @@
 # Copyright (C) 2025 ibrar-syed <syed.ibraras@gmail.com>
 # This file is part of the Skin Cancer Detection Project.
-###
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
